2f80cb2a88281ba6775ed62d6b01f1eecc28748d
John Schimmel <john.schimmel@gmail.com>
2013-02-21 22:56:44 -0500

    adding nodemonignore

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.configure(function(){
   app.engine('html', require('hogan-express')); // https://github.com/vol4ok/hogan-express
 
   app.use(express.favicon());
-  app.use(express.logger('dev'));
+  // app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
